f1f4ba8a5f0dda4c00e20102acab8c6cf71b74d2
Victor Santos <victor.santos@ipsoft.com>
2017-07-24 17:00:54 -0400

    Add comments done

--- src/components/Single.js
+++ src/components/Single.js
@@ -2,22 +2,44 @@ import React  from 'react';
 import Photo from './Photo';
 import Comments from './Comments';
 
-import { Container, Grid, Form } from 'semantic-ui-react';
+import { Container, Grid, Form, Divider } from 'semantic-ui-react';
 
 class Single extends React.Component{
     constructor(props) {
         super(props);
-
+        this.state = {
+            author: '',
+            comment: '',
+            authorError:' ',
+            commentError:' '
+        }
         this.onSummit = this.onSummit.bind(this);
     }
+
+    handleChange = (e, {name, value}) => this.setState({[name]: value});
 
     onSummit(e){
         e.preventDefault();
         const { postId } = this.props.params;
-        const author = this.refs.author.value;
-        const comment = this.refs.comment.value;
+        const author = this.state.author;
+        const comment = this.state.comment;
+
+        if (!this.validate()) return;
 
         this.props.addComment(postId, author, comment);
+        this.setState({author: '', comment: ''});
+    }
+
+    validate = () =>{
+        var validation = true;
+        ['author', 'comment'].forEach((field)=>{
+           if(this.state[field] === ''){
+               const state = `${field}Error`;
+               this.setState({[state]: 'error'});
+               validation = false;
+           }
+        });
+        return validation;
     }
 
     render(){
@@ -35,11 +57,11 @@ class Single extends React.Component{
                     </Grid.Column>
                     <Grid.Column width="6">
                         <Comments {...this.props} postComments = {postComments} />
-
-                        <Form onSubmit={this.onSummit}>
-                            <input  type="text" placeholder="author" ref="author" />
-                            <input  type="text" placeholder="comment" ref="comment" />
-                            <Form.Button type="submit" value="test" />
+                        <Divider/>
+                        <Form onSubmit={this.onSummit} ref ={(form)=> this.commentForm = form }>
+                            <Form.Input  type="text" placeholder="author" name ="author" value = {this.state.author} onChange ={this.handleChange}   />
+                            <Form.Input  type="text" placeholder="comment" name ="comment" value = {this.state.comment} onChange = {this.handleChange}   />
+                            <Form.Button type="submit">Add Comment</Form.Button>
                         </Form>
                     </Grid.Column>
                 </Grid>
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -1,14 +1,35 @@
 
-function comments(state = [], action){
-
+function postCommets(state=[], action)
+{
     switch (action.type){
         case 'ADD_COMMENT':
+            return[
+                ...state,{
+                user: action.author,
+                text: action.comment
+                }
+            ];
 
+        case 'REMOVE_COMMENT':
+            return state;
 
         default:
             return state;
-
     }
+    return state;
+}
+function comments(state = [], action){
+
+    if(typeof action.postId !== 'undefined'){
+        return {
+            //take the current state
+            ...state,
+            //overwrite the comment with a ne one ERN_SDININ: ["TEST", "COOl"]
+            [action.postId]: postCommets(state[action.postId], action)  //<-- reducer composition
+
+        }
+    }
+
     return state;
 }
 
